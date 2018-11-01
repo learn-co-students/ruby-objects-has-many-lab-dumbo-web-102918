82e234c3b488c6ab8f1badb353c5c986105ce1b3
LaTarisha Fountain <lfountain@njcu.edu>
2018-11-01 15:16:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,10 @@
 class Artist
   attr_accessor :name, :song, :songs
 
-  @@song_count = []
-  
+ 
   def initialize(name)
     @name = name
     @songs = []
-   @@song_count << @songs
   end
   
   def add_song(song)
